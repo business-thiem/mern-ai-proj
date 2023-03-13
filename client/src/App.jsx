6721cd724d12a83cba956c9ffc4a0cd7bdfcfc90
Thiem <thiem.huynh0@gmail.com>
2023-03-13 17:10:38 -0400

    folder structure setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { logo } from './assets';
 
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
-  )
-}
+  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+};
 
-export default App
+export default App;
